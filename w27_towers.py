34c0dfb17bb1a6fe06a8cc8cfc2f7b1b59453a33
seanlin <144734105+seanlin27@users.noreply.github.com>
2024-05-01 14:45:44 -0400

    Add files via upload

---
+++ w27_towers.py
@@ -0,0 +1,11 @@
+def hanoi(n, start, middle, end):
+    if n == 3:
+        print(
+        start to end
+        start to middle
+        end to middle
+        start to end
+        middle to start
+        middle to end
+        start to end)
+hanoi(3, 0, 1, 2)
